5cfd77b37bbe11d0064983345683e483b263e224
mardini-kompronk <mardini1414@gmail.com>
2022-05-07 17:19:05 +0700

    remove meta http equiv

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,10 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import NextNProgress from 'nextjs-progressbar';
 import nprogress from 'nprogress';
 import 'animate.css';
-import Head from 'next/head';
 
 nprogress.configure({ showSpinner: false });
 
-axios.defaults.baseURL = 'http://portfolioweb-backend.000webhostapp.com';
+axios.defaults.baseURL = 'https://portfolioweb-backend.000webhostapp.com';
 axios.defaults.withCredentials = true;
 axios.defaults.validateStatus = (status) => status < 500;
 
@@ -16,12 +15,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <NextNProgress color="rgb(59 130 246)" />
-      <Head>
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
